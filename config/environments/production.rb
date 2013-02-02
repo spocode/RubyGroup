e6cd85af9f317f55c296eb40e0451e71e943e2e5
David Schmidt <davids@tower-mt.com>
2013-02-02 14:40:42 -0800

    Change email method to sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails3MongoidDevise::Application.configure do
   config.action_mailer.default_url_options = { :host => 'example.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
